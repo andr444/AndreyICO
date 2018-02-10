c8702f8349ed90a666f7e6641599993b5a324e33
Andrey Dulkin <andrey@Andreys-MacBook-Pro.local>
2018-02-10 20:01:34 +0200

    trying to connect to ROPSTEN

--- truffle.js
+++ truffle.js
@@ -3,9 +3,15 @@ module.exports = {
   // to customize your Truffle configuration!
   networks: {
     development: {
-      host: "127.0.0.1",
-      port: 7545,
-      network_id: "*" // Match any network id
+        host: "127.0.0.1",
+        port: 7545,
+        network_id: "*" // Match any network id
+    },
+    ropsten:  {
+        network_id: 3,
+        host: "localhost",
+        port:  8545,
+        gas:   500000
     }
   }
 };
